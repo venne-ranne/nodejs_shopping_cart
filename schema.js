ac1bafcc1336b084973192786731029c1730ddb6
forancono <forancono@ecs.vuw.ac.nz>
2017-05-22 15:05:23 +1200

    Setup script for products database - Added node modules for postgresql and body parser - Routes defined in server.js

---
+++ schema.js
@@ -0,0 +1,29 @@
+// This script is to set up the psql database with appropriate tables
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL;
+
+var client = new pg.Client(connectionString);
+client.connect();
+
+
+/* Create main product database
+    varchar(255) : Product name
+    varchar(1024) : Product description
+    numeric : Product price
+    boolean : New Product
+    boolean : Sale Product
+    integer : Product quantity
+    varchar(128) : Product category
+    varchar(255) : Path to image resource
+*/
+var makeProductTable = client.query('create table products (name varchar(255), ' +
+                                                'description varchar(1024), ' +
+                                                'price numeric, ' +
+                                                'new boolean, ' +
+                                                'sale boolean, ' +
+                                                'stock integer, ' +
+                                                'category varchar(255), ' +
+                                                'imagePath varchar(255))');
+makeProductTable.on('end', function(result) {
+    client.end();
+});
--- server.js
+++ server.js
@@ -1,14 +1,66 @@
+// imports
 var express = require('express');
-var port = 5555;
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
+// server parameters
+var connectionString = process.env.DATABASE_URL;
+var port = process.env.PORT;
+
+// start express application
 var app = express();
+
+// connect to postgresql database
+var client = new pg.Client(connectionString);
+client.connect();
+
+// setup body parser to use JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // root page
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
-})
+});
+
+// shopping cart page
+app.get('/cart') {
+
+});
+
+// retrive all items in stock
+app.get('/collections') {
+
+});
+
+// retrive the list of all items in category1
+app.get('/collections/category1') {
+
+});
+
+// retrive the list of all items in category2
+app.get('/collections/category2') {
+
+});
+
+// retrive the list of all items in category3
+app.get('/collections/category3') {
+
+});
+
+// login request
+app.get('/login') {
+
+});
+
+// register request
+app.get('/register') {
+
+});
 
 // start server listening
 app.listen(port, function(error) {
     console.log('Listening on port ' + port);
 });
+
+// resource requests
