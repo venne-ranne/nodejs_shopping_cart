f73766156561b30ebf8c56ae17b6a92639b8f2d0
venne-ranne <vivienneyapp@gmail.com>
2017-06-02 10:30:32 +1200

    modified index.html for products layout

--- views/js/login.js
+++ views/js/login.js
@@ -98,6 +98,25 @@ $(document).ready(function(e) {
     $('#menu-new').on('click', function(){
       console.log("New button in the nav is clicked.");
       $('.home-features').load("collections.html");
+      $.ajax({
+    		type: 'GET',
+    		url: '/collections',
+    		success: function(data){
+    			for (i = 0; i < data.length; i++) {
+    				var productId = 'product_id'+data[i].id;  // add the id to the div
+            var imagepath = ''+data[i].imagepath;     // add the imagepath to src
+            var productHTML = '<div class="products-list_single" id = "'+productId+'">';
+            productHTML += '<img class = "product-image" width = "250px" height = "250px" src ="'+imagepath+'">';
+            productHTML += '<span class = "product-name"></span>';
+            productHTML += '<span class = "product-price"></span>';
+            productHTML += '<input type="button" value="ADD TO CART" class="cart-submit">';
+            var $newProduct = $(productHTML);
+            $newProduct.find('.product-name').text(data[i].name);
+            $newProduct.find('.product-price').text(data[i].price);
+            $('.products-list').prepend($newProduct);
+    			}
+    		}
+    	});
       //$('.home-features').replaceWith($('.products-list-wrap'));
     });
 
