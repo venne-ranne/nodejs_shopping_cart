fbed70271997af420c45fd809a132d7f997628fe
venne-ranne <vivienneyapp@gmail.com>
2017-06-10 22:06:09 +1200

    try to resolve some errors on heroku, uncomment the connectionString

--- views/js/shopping-cart.js
+++ views/js/shopping-cart.js
@@ -3,17 +3,17 @@ var shoppingCartNumber = undefined;
 $(document).ready(function(e) {
 
     //when page is loaded make a request to the server to a shopping cart number
-    // $.ajax({
-    //     method: 'POST',
-    //     url: '/cart',
-    //     success: function(data) {
-    //         shoppingCartNumber = data.cartid;
-    //         console.log('Shopping cart number = ' + shoppingCartNumber);
-    //     },
-    //     error: function(jqXHR, textStatus, errorThrown) {
-    //         console.log('Server failed to provide shopping cart number');
-    //     }
-    // });
+    $.ajax({
+        method: 'POST',
+        url: '/cart',
+        success: function(data) {
+            shoppingCartNumber = data.cartid;
+            console.log('Shopping cart number = ' + shoppingCartNumber);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Server failed to provide shopping cart number');
+        }
+    });
 
     // pop-up shopping cart dialog box
     $('.shopping-cart-container').dialog({
