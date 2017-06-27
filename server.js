9094dafdca3fc7a448327ab32405cd22763119af
forancono <forancono@ecs.vuw.ac.nz>
2017-06-28 10:15:18 +1200

    Bugfixing and Heroku

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ var pg = require('./config/database');
 var https = require("https");
 var fs = require("fs");
 
+// Include route modules
+var users = require('routes/users');
+var collections = require('routes/collections');
+var carts = require('routes/carts');
+
 var cert = fs.readFileSync("cert.pem");
 var key = fs.readFileSync("key.pem");
 
@@ -25,21 +30,17 @@ var port = process.env.PORT || 8080; ;
 // start express application
 var app = express();
 
-
 // static files such as css, js, resource files in views folder
 app.use(express.static('./views'));
 
-// use included routes
+/* Use Included Routes */
+
 // user registration/login
 app.use('/', users);
 // interacting with collections resource
 app.use('/collections', collections);
 // shopping cart functionality
 app.use('/carts', carts);
-
-
-//start server listening
-
 
 app.use(session({
     secret: 'iloveblackrabbitproject',
@@ -51,13 +52,13 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
 
-
 //https used for secure communication however only works local wont work when using heroku
 
 // https.createServer(obj,app).listen(port, function() {
 //     console.log("Server running on port : " + port);
 // });
 
+//start server listening
 app.listen(port, function() {
      console.log("Server running on port : " + port);
  });
