640a2c6b85e58ee3c6aef51c691f5f99563f4451
forancono <forancono@ecs.vuw.ac.nz>
2017-07-05 09:13:50 +1200

    reinstated cache on static content

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// imports
+// Imports
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var expressLayouts = require('express-ejs-layouts');
 
-// build the connection pool to manage
+// Build the connection pool to manage
 var pg = require('./config/database');
 
-//manage https secure connection
+// Manage https secure connection
 var https = require("https");
 var fs = require("fs");
 
@@ -25,15 +25,16 @@ var obj = {
 };
 
 var port = process.env.PORT || 8080; ;
+// Time to cache static content
 var maxCacheTime = 1000 * 60 * 2;
 
-// start express application
+// Start express application
 var app = express();
 
-// static files such as css, js, resource files in views folder
+// Static files such as css, js, resource files in views folder
 // set length of time to cache
 app.use(express.static('./views', {
-//  maxAge: maxCacheTime
+    maxAge: maxCacheTime
 }));
 
 /* Use Included Routes */
@@ -45,26 +46,21 @@ app.use('/collections', collections);
 // shopping cart functionality
 app.use('/carts', carts);
 
-
-
 // set up template engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
 
-
-// // force http to https
+// Force http to https
 app.use(function (request, response, next) {
     if (!request.headers.host.startsWith("localhost") && request.headers['x-forwarded-proto'] !== 'https') {
         var httpsUrl = ['https://guarded-falls-74429.herokuapp.com/', request.url].join('');
         return response.redirect(httpsUrl);
     }
-
     return next();
 });
 
 //https used for secure communication however only works local wont work when using heroku
-
 /*
  https.createServer(obj,app).listen(port, function() {
      console.log("Server running on port : " + port);
@@ -77,7 +73,7 @@ app.get('/', function(req, res) {
     res.render('index.ejs', {totalcart: 0});
 });
 
-//start server listening
+// start server listening
 app.listen(port, function() {
      console.log("Server running on port : " + port);
 
